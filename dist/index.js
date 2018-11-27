abca1845f93bed348bfcafebeb3d8a87f23991d4
ardean <aron.stifani@appklusiv.ch>
2018-11-27 19:26:15 +0100

    v0.2.1 - fixed app version, added history fallback

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const api_1 = require("./api");
+const util_1 = require("util");
+const path = require("path");
 const express = require("express");
 const scheduler_1 = require("./scheduler");
-const path = require("path");
-const util_1 = require("util");
-const api_1 = require("./api");
+const fallback = require("express-history-api-fallback");
 const PORT = 2222;
 const APP_PATH = path.resolve(path.dirname(require.resolve("jsschedule-app/package.json")), "build");
 const app = express();
 app.use(express.static(APP_PATH));
 app.disable("x-powered-by");
 app.use("/api", api_1.default);
+app.use(fallback(`${APP_PATH}/index.html`));
 app.listen(PORT, async () => {
     await scheduler_1.default.init();
     util_1.log("jsschedule listening on port " + PORT);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import api from "./api";
+import { log } from "util";
+import * as path from "path";
 import * as express from "express";
 import scheduler from "./scheduler";
-import * as path from "path";
-import { log } from "util";
-import api from "./api";
+import * as fallback from "express-history-api-fallback";
 
 const PORT = 2222;
 const APP_PATH = path.resolve(path.dirname(require.resolve("jsschedule-app/package.json")), "build");
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.static(APP_PATH));
 app.disable("x-powered-by");
 app.use("/api", api);
+app.use(fallback(`${APP_PATH}/index.html`));
 app.listen(PORT, async () => {
   await scheduler.init();
   log("jsschedule listening on port " + PORT);
